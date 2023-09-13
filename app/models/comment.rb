5ce31f837598a270e4574cc0ffcb9079b8c23912
lokeshk88 <142203160+lokeshk88@users.noreply.github.com>
2023-09-13 05:26:26 +0000

    comiit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
+  validate_presense_of :body , message: "Comment field not be empty !!!"
+  #Proc.new { | user, data |"#{data[:attribute]} is needed for all registrations!" } 
 end
